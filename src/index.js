740af17fd69cdc9339583874979d256f1ef04d5c
mmazepa <maksymilianmariusz@gmail.com>
2020-10-09 01:42:02 +0200

    Przepisanie komponentu Board - wszystkie pola za pomocą dwóch pętli zamiast jawnego podawania w kodzie.

--- src/index.js
+++ src/index.js
@@ -25,24 +25,22 @@ class Board extends React.Component {
         );
     }
 
+    renderBoard() {
+        let board = [];
+        for (var i = 0; i < 3; i++) {
+            let squares = [];
+            for (var j = 0; j < 3; j++) {
+                squares.push(this.renderSquare(3*i+j));
+            }
+            board.push(<div className="board-row">{squares}</div>);
+        }
+        return board;
+    }
+
     render() {
         return (
             <div>
-                <div className="board-row">
-                    {this.renderSquare(0)}
-                    {this.renderSquare(1)}
-                    {this.renderSquare(2)}
-                </div>
-                <div className="board-row">
-                    {this.renderSquare(3)}
-                    {this.renderSquare(4)}
-                    {this.renderSquare(5)}
-                </div>
-                <div className="board-row">
-                    {this.renderSquare(6)}
-                    {this.renderSquare(7)}
-                    {this.renderSquare(8)}
-                </div>
+                {this.renderBoard()}
             </div>
         );
     }
