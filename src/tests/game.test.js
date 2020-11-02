1a85ccc1fad6fe0d3633024e31834eaf4be15927
mmazepa <maksymilianmariusz@gmail.com>
2020-11-02 22:39:46 +0100

    Testowania dalszy ciąg.

--- src/tests/game.test.js
+++ src/tests/game.test.js
@@ -1,12 +1,20 @@
 /* jshint esversion: 6 */
 
 import React from "react";
-import { render } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+import { render, screen } from "@testing-library/react";
 
 import Game from "./../components/game.js";
 
 describe("game", () => {
   test("Renders game component.", () => {
     render(<Game />);
+  });
+
+  test("Search for the initial text to be in the document.", () => {
+    render(<Game />);
+    expect(screen.getByText("Teraz gracz: ✗")).toBeInTheDocument();
+    expect(screen.getByText("Sortuj")).toBeInTheDocument();
+    expect(screen.getByText("Przejdź na początek gry")).toBeInTheDocument();
   });
 });
